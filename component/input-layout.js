f840ff67795f4679101bfd495a83be17615dbaff
sumariana <42775471+sumariana@users.noreply.github.com>
2021-02-09 18:03:04 +0800

    asd

---
+++ component/input-layout.js
@@ -0,0 +1,136 @@
+import React,{ useEffect, useReducer,useState } from 'react'
+import { StyleSheet, View, Text,TextInput } from 'react-native'
+import { Icon } from 'react-native-elements'
+
+const INPUT_CHANGE ='INPUT_CHANGE';
+const INPUT_BLUR = 'INPUT_BLUR'
+
+const inputReducer = (state,action) =>{
+    switch(action.type){
+        case INPUT_CHANGE: return{
+            ...state,
+            value: action.value,
+            isValid: action.isValid,
+            errorMessage: action.errorMessage
+        }
+        case INPUT_BLUR: return{
+            ...state,
+            touched : true
+        }
+        default : return state;
+    }
+};
+
+const TextInputLayout = props =>{
+
+    const [inputState,dispatchInput] = useReducer(inputReducer,{
+        value: props.initialValue ? props.initialValue : '',
+        isValid: props.initialValidated,
+        errorMessage: '',
+        touched: false
+    });
+
+    const textChangeHandler = text =>{
+        const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        let isValidated = true;
+        let errorMessage = ''
+        if (props.required && text.trim().length === 0) {
+            isValidated = false;
+            errorMessage = 'field can\'t be empty'
+        }
+        if (props.email && !emailRegex.test(text.toLowerCase())) {
+            isValidated = false;
+            errorMessage = 'Email format is wrong'
+        }
+        if (props.min != null && +text < props.min) {
+            isValidated = false;
+            errorMessage = 'field must be at least'.concat(props.min).concat('character')
+        }
+        if (props.max != null && +text > props.max) {
+            isValidated = false;
+            errorMessage = 'field max'.concat(props.min).concat('character')
+        }
+        if (props.minLength != null && text.length < props.minLength) {
+            isValidated = false;
+            errorMessage = 'field must be at least'.concat(props.min).concat('character')
+        }
+
+        dispatchInput({type: INPUT_CHANGE,
+        value: text,
+        isValid: isValidated,
+        errorMessage: errorMessage
+        })
+    };
+
+    // const { onInputChange } = props
+
+    // useEffect(()=>{
+    //     if(inputState.touched){
+    //         onInputChange(inputState.value, inputState.isValid)
+    //     }
+    // },[inputState,onInputChange])
+
+    const lostFocusHandler = () =>{
+        dispatchInput({
+            type: INPUT_BLUR
+        })
+    }
+
+    const [secure, setSecure] = useState(props.isPassword!=null ? true : false)
+
+    return(
+        <View style={styles.container}>
+            <Text style={styles.label}>{props.label}</Text>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    {...props}
+                    style={styles.input}
+                    value={inputState.value}
+                    onChangeText={textChangeHandler}
+                    onBlur={lostFocusHandler}
+                    placeholder={props.label}
+                    secureTextEntry = {secure}
+                />
+                {
+                props.isPassword &&
+                <Icon
+                containerStyle={{paddingEnd: 10}}
+                name={secure ? 'eye' : 'eye-slash'}
+                type = 'font-awesome'
+                onPress={() => setSecure(!secure)} />
+                }
+            </View>
+            {!inputState.isValid && inputState.touched && (
+            <Text>
+                {inputState.errorMessage}
+            </Text>)}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container:{
+        flexDirection: 'column'
+    },
+    label: {
+        fontSize:18,
+        color: 'black',
+        marginStart:20,
+        paddingVertical:5
+    },
+    inputContainer:{
+        width: '100%',
+        flexDirection:'row',
+        alignItems:'center',
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 20,
+    },
+    input: {
+        flex:1,
+        fontSize:18,
+        paddingHorizontal:20,
+        paddingVertical:5
+    }
+});
+export default TextInputLayout;
--- screens/LoginScreens.js
+++ screens/LoginScreens.js
@@ -1,11 +1,52 @@
 import React from 'react'
 import { StyleSheet, View, Text,Image,ScrollView } from 'react-native'
+import { Button } from 'react-native-elements';
+
+import TextInputLayout from '../component/input-layout'
 
 const LoginScreen = props =>{
     return (
         <ScrollView contentContainerStyle = {styles.screen}>
             <View style={styles.imageContainer}>
                 <Image style={styles.Image} source={require('../assets/images/logo.png')}/>
+            </View>
+            <View style={styles.inputContainer}>
+                <TextInputLayout
+                label = 'username'
+                />
+                <TextInputLayout
+                label = 'password'
+                isPassword = {true}
+                />
+            </View>
+            <View style={styles.buttonContainer}>
+            <Button
+                title='LOGIN'
+                containerStyle={{marginTop:50,width:'100%'}}
+                buttonStyle={{borderRadius:20,backgroundColor:'#F3717F',marginHorizontal:20}}
+                titleStyle={{fontSize:22}}
+                />
+                <Text style={{fontSize:16,marginTop:10,marginBottom:20}}>Don't have an account? 
+                    <Text style={{color: '#F3717F'}} 
+                      onPress={()=>console.log('we are pressing register')}>
+                      Register Now!
+                    </Text> 
+                </Text>
+            <View style={{
+                borderStyle: 'dotted',
+                borderColor: 'gray',
+                borderWidth: 1,
+                height:1,
+                width:'100%',
+                borderRadius: 1,
+            }}>
+            </View>
+            <Button
+                title='Sign in with another method'
+                containerStyle={{marginTop:30,width:'100%'}}
+                buttonStyle={{borderRadius:20,backgroundColor:'black',marginHorizontal:20}}
+                titleStyle={{fontSize:22}}
+                />
             </View>
         </ScrollView>
     );
@@ -15,16 +56,23 @@ const styles = StyleSheet.create({
     screen:{
         flexGrow:1
     },
+    inputContainer:{
+        marginTop: '15%',
+        marginHorizontal:20
+    },
     imageContainer:{
         width:'100%',
-        height: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderWidth:1,
-        borderColor:'black'
+        marginTop: '20%'
     },
     Image:{
-        width:'80%'
+        width:'100%',
+        resizeMode:'contain'
+
+    },
+    buttonContainer:{
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center'
     }
 });
 
