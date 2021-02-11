fdb4889ae2fe9d5b7d8230dbad49f456648176bf
sumariana <sumarianakadek@gmail.com>
2021-02-11 17:59:39 +0800

    Update recipe-action.js

--- store/recipe/recipe-action.js
+++ store/recipe/recipe-action.js
@@ -1,10 +1,25 @@
 export const SET_FAVORITE = 'SET_FAVORITE';
-export const GET_RECIPE = 'GET_RECIPE'
+export const REMOVE_FAV= 'REMOVE_FAV'
+export const GET_RECIPE = 'GET_RECIPE';
+export const GET_FAV = 'GET_FAV';
+export const GET_DETAIL_RECIPE = 'DETAIL_RECIPE';
 
 export const setFavorite = (data) =>{
     return {type: SET_FAVORITE,recipe=data}
 }
 
+export const removeFavorite = (data) =>{
+    return {type: REMOVE_FAV,recipe = data}
+}
+
+export const getFavoriteList = () =>{
+    return {type: GET_FAV}
+}
+
 export const getRecipeList = () =>{
     return {type: GET_RECIPE}
+}
+
+export const getRecipeDetail = (data)=>{
+    return {type: GET_DETAIL_RECIPE,recipe = data}
 }
