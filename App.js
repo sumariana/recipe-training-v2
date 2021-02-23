1879615d4a565655554833105ea40a0652127b50
sumariana <42775471+sumariana@users.noreply.github.com>
2021-02-23 09:19:20 +0800

    done

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect,useCallback} from 'react';
 import AppLoading from 'expo-app-loading';
 import * as Font from 'expo-font';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider, useSelector } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
+import {ActivityIndicator,View} from 'react-native'
 
 import authReducer from './store/auth/auth-reducers';
 import recipeReducer from './store/recipe/recipe-reducers';
@@ -15,29 +16,42 @@ const rootReducer = combineReducers({
   recipeReducer:recipeReducer
 });
 
-const fetchFont = () => {
-  return Font.loadAsync({
-      'roboto-medium': require('./assets/fonts/Roboto-Medium.ttf'),
-      'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf'),
-      'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
-  })
-};
-
 const store = createStore(rootReducer,applyMiddleware(ReduxThunk))
 
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
 
-  if (!fontLoaded) {
+  // if (!fontLoaded) {
+  //   return (
+  //       <AppLoading
+  //           startAsync={fetchFont}
+  //           onError={ err => {} }
+  //           onFinish={() => {
+  //               setFontLoaded(true)
+  //       }}/>
+  //   )
+  // }
+
+  const loadFonts = useCallback(async()=>{
+    await Font.loadAsync({
+      'roboto-medium': require('./assets/fonts/Roboto-Medium.ttf'),
+      'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf'),
+      'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    })
+    setFontLoaded(true)
+  },[])
+
+  useEffect(()=>{
+    loadFonts()
+  },[])
+
+  if(!fontLoaded){
     return (
-        <AppLoading
-            startAsync={fetchFont}
-            onError={ err => {} }
-            onFinish={() => {
-                setFontLoaded(true)
-        }}/>
-    )
-}
+      <View style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'white'}}>
+            <ActivityIndicator size="large" color="#F3717F" />
+      </View>
+    );
+  }
 
   return (
     <Provider store = {store}>
